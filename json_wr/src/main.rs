f6eb2ffd06211680f7b16c9b6d75e31e6a0a6375
dika <dika@jagoanhosting.com>
2022-11-07 12:06:56 +0700

    add example json data

---
+++ json_wr/src/main.rs
@@ -0,0 +1,34 @@
+use serde::{Serialize, Deserialize};
+use serde_json::Result;
+
+#[derive(Serialize, Deserialize)]
+struct Person {
+    name: String,
+    age: u8,
+    phones: Vec<String>,
+}
+
+fn untyped_example() -> Result<()> {
+    // Some JSON input data as a &str. Maybe this comes from the user.
+    let data = r#"
+        {
+            "name": "John Doe",
+            "age": 43,
+            "phones": [
+                "+44 1234567",
+                "+44 2345678"
+            ]
+        }"#;
+
+    // Parse the string of data into serde_json.
+    let p: Person = serde_json::from_str(data)?;
+
+    // Do things just like with any other Rust data structure.
+    println!("Please call {} at the number {}", p.name, p.phones[0]);
+
+    Ok(())
+}
+
+fn main() {
+    untyped_example().unwrap();
+}
